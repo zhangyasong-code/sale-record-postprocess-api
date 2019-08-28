d7c6a750d6cab15c788a373d37e5f5418d5a2966
Jang Jaehue <jaehue@jang.io>
2019-08-28 13:20:21 +0800

    #20 Use echomiddleware.ContextDBType

--- factory/factory.go
+++ factory/factory.go
@@ -4,11 +4,12 @@ import (
 	"context"
 
 	"github.com/go-xorm/xorm"
+	"github.com/pangpanglabs/goutils/echomiddleware"
 )
 
 var (
-	SaleRecordDBContextName = "saleRecordDB"
-	OrderDBContextName      = "orderDB"
+	SaleRecordDBContextName echomiddleware.ContextDBType = "saleRecordDB"
+	OrderDBContextName      echomiddleware.ContextDBType = "orderDB"
 )
 
 func SaleRecordDB(ctx context.Context) xorm.Interface {
