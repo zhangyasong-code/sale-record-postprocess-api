4ad1c700ff1e0433559c2c2ab8ebc4ae23db9be8
程翠翠(북경개발센터 정보시스템실) <Cheng.CuiCui@home.e-kmall.com>
2019-09-04 14:22:12 +0800

    #31 add gradeId、brandId & change SaleRecordMstId to TransactionId

--- models/mileage.go
+++ models/mileage.go
@@ -16,8 +16,8 @@ import (
 type Mileage struct {
 	Id         int64      `json:"id,omitempty"`         /*ID 主键*/
 	TenantCode string     `json:"tenantCode,omitempty"` /*租户代码*/
-	MallId     string     `json:"mallId,omitempty"`     /*购物中心代码*/
-	StoreId    string     `json:"storeId,omitempty"`    /*店铺代码*/
+	MallId     int64      `json:"mallId,omitempty"`     /*购物中心代码*/
+	StoreId    int64      `json:"storeId,omitempty"`    /*店铺代码*/
 	MemberId   int64      `json:"memberId"`             /*会员id*/
 	Type       string     `json:"type,omitempty"`       /*类型*/
 	ChannelId  int64      `json:"channelId"`            /*渠道*/
@@ -33,6 +33,24 @@ type Mileage struct {
 	UpdatedBy  string     `json:"updatedBy,omitempty"`
 	CreatedAt  *time.Time `json:"createdAt,omitempty"`
 	UpdatedAt  *time.Time `json:"updatedAt,omitempty"`
+}
+
+type MileageMall struct {
+	TenantCode             string     `json:"tenantCode,omitempty"`   /*租户代码*/
+	MallId                 int64      `json:"mallId,omitempty"`       /*购物中心代码*/
+	MemberId               int64      `json:"memberId,omitempty"`     /*会员id*/
+	CardTypeId             int64      `json:"cardTypeId"`             /*卡片类型*/
+	GradeId                int64      `json:"gradeId"`                /*等级*/
+	PreExitType            int64      `json:"PreExitType"`            /*预退出顾客类型，适用于eland集团5、6等级顾客*/
+	Point                  float64    `json:"point"`                  /*总积分数量*/
+	TotalAmount            float64    `json:"totalAmount"`            /*交易总金额*/
+	TotalCount             int64      `json:"totalCount"`             /*消费总笔数*/
+	RecentPurchaseDatetime *time.Time `json:"recentPurchaseDatetime"` /*最近消费时间*/
+	OldMobile              string     `json:"oldMobile,omitempty"`
+	CreatedBy              string     `json:"createdBy,omitempty"`
+	UpdatedBy              string     `json:"updatedBy,omitempty"`
+	CreatedAt              *time.Time `json:"createdAt,omitempty"`
+	UpdatedAt              *time.Time `json:"updatedAt,omitempty"`
 }
 
 func (Mileage) GetMembershipMileages(ctx context.Context, tradeNo int64, mileageType string) ([]Mileage, error) {
@@ -65,4 +83,32 @@ func (Mileage) GetMembershipMileages(ctx context.Context, tradeNo int64, mileage
 	}
 
 	return resp.Result.Items, nil
+}
+
+func (MileageMall) GetMembershipGrade(ctx context.Context, brandId, memberId int64, tenantCode string) (*MileageMall, error) {
+	var resp struct {
+		Result  *MileageMall `json:"result"`
+		Success bool         `json:"success"`
+		Error   struct {
+			Code    int    `json:"code"`
+			Message string `json:"message"`
+			Details string `json:"details"`
+		} `json:"error"`
+	}
+	url := fmt.Sprintf("%s/v1/mileage-mall?tenantCode=%s&mallId=%v&memberId=%v",
+		config.Config().Services.MembershipApi, tenantCode, brandId, memberId)
+	logrus.WithField("url", url).Info("url")
+	if _, err := httpreq.New(http.MethodGet, url, nil).
+		WithBehaviorLogContext(behaviorlog.FromCtx(ctx)).
+		Call(&resp); err != nil {
+		return nil, err
+	} else if !resp.Success {
+		logrus.WithFields(logrus.Fields{
+			"errorCode":    resp.Error.Code,
+			"errorMessage": resp.Error.Message,
+		}).Error("Fail to get mileage_mall")
+		return nil, fmt.Errorf("[%d]%s", resp.Error.Code, resp.Error.Details)
+	}
+
+	return resp.Result, nil
 }
